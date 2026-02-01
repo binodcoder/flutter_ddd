8fdb24a78398a8ea8e238889821b2677155b98e0
Binod Bhandari <binodcoder@gmail.com>
2026-02-01 11:53:14 +0000

    refactor sign in form bloc

--- lib/application/auth/sign_in_form/sign_in_form_bloc.dart
+++ lib/application/auth/sign_in_form/sign_in_form_bloc.dart
@@ -35,7 +35,7 @@ class SignInFormBloc extends Bloc<SignInFormEvent, SignInFormState> {
   ) async {
     emit(
       state.copyWith(
-        emailAddress: EmailAddress(event.passwordStr),
+        password: Password(event.passwordStr),
         authFailureOrSuccessOption: none(),
       ),
     );
@@ -59,44 +59,43 @@ class SignInFormBloc extends Bloc<SignInFormEvent, SignInFormState> {
     RegisterWithEmailAndPasswordPressed event,
     Emitter<SignInFormState> emit,
   ) async {
-    final isEmailValid = state.emailAddress.isValid();
-    final isPasswordValid = state.password.isValid();
-
-    if (isEmailValid && isPasswordValid) {
-      emit(state.copyWith(isSubmitting: true, authFailureOrSuccessOption: none()));
-
-      final failureOrSuccess = await _authFacade.registerWithEmailAndPassword(
-        emailAddress: state.emailAddress,
-        password: state.password,
-      );
-
-      emit(
-        state.copyWith(
-          isSubmitting: false,
-          authFailureOrSuccessOption: some(failureOrSuccess),
-        ),
-      );
-    }
-    emit(state.copyWith(showErrorMessages: true, authFailureOrSuccessOption: none()));
+    await _performActionOnAuthFacadeWithEmailAndPassword(
+      emit,
+      _authFacade.registerWithEmailAndPassword,
+    );
   }
 
   Future<void> _onSignInWithEmailAndPasswordPressed(
     SignInWithEmailAndPasswordPressed event,
     Emitter<SignInFormState> emit,
   ) async {
-    late Either<AuthFailure, Unit> failureOrSuccess;
+    await _performActionOnAuthFacadeWithEmailAndPassword(
+      emit,
+      _authFacade.signInWithEmailAndPassword,
+    );
+  }
 
+  Future<void> _performActionOnAuthFacadeWithEmailAndPassword(
+    Emitter<SignInFormState> emit,
+    Future<Either<AuthFailure, Unit>> Function({
+      required EmailAddress emailAddress,
+      required Password password,
+    })
+    forwardedCall,
+  ) async {
+    late Either<AuthFailure, Unit> failureOrSuccess;
     final isEmailValid = state.emailAddress.isValid();
     final isPasswordValid = state.password.isValid();
 
     if (isEmailValid && isPasswordValid) {
       emit(state.copyWith(isSubmitting: true, authFailureOrSuccessOption: none()));
 
-      failureOrSuccess = await _authFacade.signInWithEmailAndPassword(
+      failureOrSuccess = await forwardedCall(
         emailAddress: state.emailAddress,
         password: state.password,
       );
     }
+
     emit(
       state.copyWith(
         isSubmitting: false,
